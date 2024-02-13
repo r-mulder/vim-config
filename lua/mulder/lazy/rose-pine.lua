5a9360d31706624479329b25d6c061af709fb694
code-capi <info@mulderr.com>
2024-02-13 13:26:00 +0100

    initial commit

---
+++ lua/mulder/lazy/rose-pine.lua
@@ -0,0 +1,16 @@
+return {
+        {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                variant = "moon",
+                styles = {
+                    transparent = true,
+                }
+            })
+
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
+}
